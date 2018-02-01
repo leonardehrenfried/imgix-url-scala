fc81b06efa04f8e12d2e1b4a2f1bbd4cff78ccdd
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2018-02-01 22:57:35 +0100

    Add ci script

---
+++ bin/ci-publish.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eu
+
+if [[ "$TRAVIS_SECURE_ENV_VARS" == true ]]; then
+  echo "Publishing..."
+  git log | head -n 20
+  echo "$PGP_SECRET" | base64 --decode | gpg --import
+
+  openssl aes-256-cbc -K $encrypted_0261605d70ea_key -iv $encrypted_0261605d70ea_iv -in travis/secrets.tar.enc -out travis/secrets.tar -d
+  tar xv -C travis -f travis/secrets.tar;
+
+  if [ -n "$TRAVIS_TAG" ]; then
+    echo "Tag push, publishing stable release to Sonatype."
+    sbt publishSigned sonatypeReleaseAll
+  else
+    echo "Merge, publishing snapshot to Sonatype."
+    sbt publishSigned
+  fi
+else
+  echo "Skipping publish, branch=$TRAVIS_BRANCH"
+fi
