db4256ef812f858f7e9f96c5c0a02c9a326014be
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2018-02-05 10:26:49 +0100

    Import gpg key in travis

--- travis/ci-publish.sh
+++ travis/ci-publish.sh
@@ -10,6 +10,8 @@ if [[ "$TRAVIS_SECURE_ENV_VARS" == true ]]; then
     sbt publishSigned sonatypeReleaseAll
   else
     echo "Merge, publishing snapshot to Sonatype."
+    openssl aes-256-cbc -K $encrypted_13ad22db1e75_key -iv $encrypted_13ad22db1e75_iv -in travis/all.gpg.enc -out travis/all.gpg -d
+    gpg --import travis/all.gpg
     sbt publish
   fi
 else
