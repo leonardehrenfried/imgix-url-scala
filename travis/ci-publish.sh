11310c72b33a0a4657f6572eb6009dd2facc25b9
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2018-02-05 11:03:55 +0100

    Add debugging help

--- travis/ci-publish.sh
+++ travis/ci-publish.sh
@@ -12,6 +12,7 @@ if [[ "$TRAVIS_SECURE_ENV_VARS" == true ]]; then
     echo "Merge, publishing snapshot to Sonatype."
     openssl aes-256-cbc -K $encrypted_13ad22db1e75_key -iv $encrypted_13ad22db1e75_iv -in travis/all.gpg.enc -out travis/all.gpg -d
     gpg --import travis/all.gpg
+    ls -la ~/.gnupg
     sbt publishSigned
   fi
 else
