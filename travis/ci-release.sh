0010167c626148bbe21c0b3df0100be40e14e436
Leonard Ehrenfried <mail@leonard.io>
2018-02-17 12:39:01 +0100

    Properly set up third step

---
+++ travis/ci-release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu
+
+if [[ "$TRAVIS_SECURE_ENV_VARS" == true ]]; then
+  echo "Publishing..."
+
+  if [ -n "$TRAVIS_TAG" ]; then
+    echo "Tag push, publishing stable release to Sonatype."
+    sbt sonatypeReleaseAll
+  else
+    echo "Not a tag, hence not releasing"
+  fi
+else
+  echo "Skipping publish, branch=$TRAVIS_BRANCH"
+fi
